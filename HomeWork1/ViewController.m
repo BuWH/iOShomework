67286517f60b3c3550d5476bd9613f0aee8da89e
wenhe <wenhe@fenbi.com>
2020-07-08 10:28:12 +0800

    first commit

--- HomeWork1/ViewController.m
+++ HomeWork1/ViewController.m
@@ -7,17 +7,29 @@
 //
 
 #import "ViewController.h"
-
+#import <Masonry/Masonry.h>
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *countLabel;
+@property (weak, nonatomic) IBOutlet UIStackView *stackView;
+@property (nonatomic) int count;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _count = 0;
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)buttonClicked:(id)sender {
+    [_countLabel setText:[NSString stringWithFormat:@"Count: %d", ++_count]];
+}
+
+- (void)viewWillLayoutSubviews {
+    [_stackView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.mas_equalTo(self.view);
+    }];
+}
 
 @end
