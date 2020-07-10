911b3302b3d384003602a0ab2c41b0f188494137
wenhe <wenhe@fenbi.com>
2020-07-10 12:03:32 +0800

    define Model

--- HomeWork2/Model/WeiboModel.h
+++ HomeWork2/Model/WeiboModel.h
@@ -7,10 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Mantle/Mantle.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WeiboModel : NSObject
+@interface WeiboTimeline : MTLModel<MTLJSONSerializing>
+
+@property (nonatomic, strong) NSArray *statuses;
+
+@end
+
+@interface WeiboUser : MTLModel<MTLJSONSerializing>
+
+@property (nonatomic, strong) NSString *screenName;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSURL *profileImageURL;
+
+@end
+
+@interface WeiboStatus : MTLModel<MTLJSONSerializing>
+
+@property (nonatomic, strong) NSString *idstr;
+@property (nonatomic, strong) NSString *text;
+@property (nonatomic, strong) WeiboUser *user;
+@property (nonatomic, strong) NSDate *createdAt;
 
 @end
 
--- HomeWork2/Model/WeiboModel.m
+++ HomeWork2/Model/WeiboModel.m
@@ -7,8 +7,67 @@
 //
 
 #import "WeiboModel.h"
-#import <Mantle/Mantle.h>
 
-@implementation WeiboModel
+
+@implementation WeiboTimeline
+
++ (NSDictionary*)JSONKeyPathsByPropertyKey {
+    return @{
+        @"statuses" : @"statuses"
+    };
+}
+
++ (NSValueTransformer*)statusesJSONTransformer {
+    return [MTLJSONAdapter arrayTransformerWithModelClass:WeiboStatus.class];
+}
+
+@end
+
+@implementation WeiboUser
+
++ (NSDictionary*)JSONKeyPathsByPropertyKey {
+    return @{
+        @"screenName" : @"screen_name",
+        @"name" : @"name",
+        @"profileImageURL" : @"profile_image_url"
+    };
+}
+
++ (NSValueTransformer*)profileImageURLJSONTransformer {
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
+@end
+
+@implementation WeiboStatus
+
++ (NSDateFormatter *)dateFormatter {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy-MM-dd";
+    return dateFormatter;
+}
+
++ (NSDictionary*)JSONKeyPathsByPropertyKey {
+    return @{
+        @"idstr" : @"idstr",
+        @"text" : @"text",
+        @"user" : @"user",
+        @"createdAt" : @"created_at"
+    };
+}
+
++ (NSValueTransformer*)userJSONTransformer {
+    return [MTLJSONAdapter dictionaryTransformerWithModelClass:WeiboUser.class];
+}
+
++ (NSValueTransformer*)createdAtJSONTransformer {
+    return [MTLValueTransformer transformerUsingForwardBlock:^id(NSString *date, BOOL *success, NSError *__autoreleasing *error) {
+        return [self.dateFormatter dateFromString: date];
+    }];
+}
+
+- (NSString*)description {
+    return [NSString stringWithFormat:@"name: %@ date: %@\n text: %@", self.user.name, self.createdAt, self.text];
+}
 
 @end
